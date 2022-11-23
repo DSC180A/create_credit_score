09633d01d41436d85642d1c0ff927ad215ee4245
nathanv1614 <nathan.vanlingen1614@gmail.com>
2022-11-23 00:42:15 -0800

    make 'test' target

--- run.py
+++ run.py
@@ -6,7 +6,7 @@ from sklearn.preprocessing import StandardScaler
 
 
 
-def main():
+def main(targets):
     '''
     Runs the main project pipeline logic, given the targets.
     targets must contain: 'data', 'analysis', 'model'. 
@@ -14,33 +14,33 @@ def main():
     `main` runs the targets in order of data=>analysis=>model.
     '''
     predictions_fp = 'predictions.csv'
-    
-    
-    data = pd.read_csv('test/testdata.csv')
+    if 'test' in targets:
+        data = pd.read_csv('test/testdata.csv')
 
-    print(data.shape)
-    
-    X_test = data.drop(columns='bad')
-    y_test = data.bad.astype(int)
+        print(data.shape)
 
-    print(X_test.shape)
+        X_test = data.drop(columns='bad')
+        y_test = data.bad.astype(int)
 
-    scaler = StandardScaler()
-    scaler.fit(X_test)
-    scaled_data = pd.DataFrame(scaler.transform(X_test), columns = X_test.columns)
-    X_test = scaled_data
-    
-    pklmodel = pkl.load(open('src/data/model.pkl', 'rb'))
-    
-    predictions = pd.Series(pklmodel.predict(X_test))
-    
-    
-    predictions.to_csv(predictions_fp, index_label=False)
+        print(X_test.shape)
 
-    return
+        scaler = StandardScaler()
+        scaler.fit(X_test)
+        scaled_data = pd.DataFrame(scaler.transform(X_test), columns = X_test.columns)
+        X_test = scaled_data
+
+        pklmodel = pkl.load(open('src/data/model.pkl', 'rb'))
+
+        predictions = pd.Series(pklmodel.predict(X_test))
+
+
+        predictions.to_csv(predictions_fp, index_label=False)
+
+        return
 
 
 if __name__ == '__main__':
     # run via:
     # python main.py data features model
-    main()
+    targets = sys.argv[1:]
+    main(targets)
